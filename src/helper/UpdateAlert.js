e70b3c5156bc5397bc15f0bc7d7a3d4a68af0266
asrafulalamazad <asrafulalamazad@gmail.com>
2023-08-29 01:00:20 +0600

    status change update

--- src/helper/UpdateAlert.js
+++ src/helper/UpdateAlert.js
@@ -5,8 +5,7 @@ export function UpdateToDO(id,status){
     return Swal.fire({
         title: 'Change Status',
         input: 'select',
-        inputOptions: {New: 'New', Completed: 'Completed', Progress: 'Progress', Canceled: 'Canceled},
-        // , Hide: 'Hide'
+        inputOptions: {New: 'New', Completed: 'Completed', Progress: 'Progress', Canceled: 'Canceled'},
         inputValue:status,
     }).then((result)=>{
         return UpdateStatusRequest(id, result.value).then((res)=>{
